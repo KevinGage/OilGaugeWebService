c54a61f0e9bd451944b83b0bf7fe8532d268ba2b
Kevin Gage <dudeman02379@gmail.com>
2017-04-03 15:10:56 -0400

    modified db.js with a couple of sample db queries.  One inserts into the user table and the other reads from it.  closes #4.  Created one get and one post example in index.js without authentication.  ref #5 ref #9

--- db.js
+++ db.js
@@ -1,14 +1,54 @@
 //Import modules
 var config = require('./private/config.js');
-var sql = require('seriate');
+var mysql = require('mysql');
 var bcrypt = require('bcrypt-nodejs');
 
 //Setup SQL Server connection
-var sqlConfig = {  
-    "server": config.dbAddress,
-    "user": config.dbUser,
-    "password": config.dbPass,
-    "database": config.dbName
-};
+var pool = mysql.createPool({
+	host		: config.dbAddress,
+	user		: config.dbUser,
+	password	: config.dbPassword,
+	database	:  config.dbName
+});
 
-sql.setDefaultConfig(sqlConfig);
+//Export SQL Queries For Users
+module.exports.users = {
+	//Inserts a new record into the users table
+	insertNew: function(userName, userPassword, userRole, userEmail, cb) {
+		if(userName === null || userPassword === null || userRole === null || userEmail === null)
+        	{
+            		return cb('username, password, role, and email cannot be null');
+        	}
+
+		bcrypt.genSalt(config.hashStrength, function(err, salt) {
+			if(err){return cb(err);}
+
+			bcrypt.hash(userPassword, salt, null, function(err, hash) {
+				if(err){return cb(err);}
+
+				pool.getConnection(function(err, connection) {
+					if(err){return cb(err);}
+
+					connection.query('INSERT INTO users (userName, userEmail, userSalt, userPass, userRole) VALUES (?, ?, ?, ?, ?)', [userName, userEmail, salt, hash, userRole], function (err, results, fields) {
+						connection.release();
+						if(err){return cb(err);}
+						return cb(null);
+					}); 
+				});
+			});
+		});
+	},
+
+	//Returns all username in database
+	returnAllNames: function(cb) {
+		pool.getConnection(function(err, connection) {
+			if(err){return cb(err);}
+
+			connection.query('SELECT userName from users', function(error, results, fields) {
+				connection.release();
+				if(err){return cb(err, null, null);}
+				return cb(null, results, fields);
+			});
+		});
+	}
+}
--- index.js
+++ index.js
@@ -8,9 +8,41 @@ var express = require('express');
 var passport = require('passport');
 var strategy = require('passport-local').Strategy;
 var app = express();
+var config = require('./private/config.js');
 
 //Setup TLS
 var credentials = {key: privateKey, cert: certificate};
+
+//Load middleware
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: config.cookieSecret, resave: false, saveUninitialized: false }));
+
+//TEST ROUTES
+app.post('/user', function (req, res) {
+  db.users.insertNew(req.body.username, req.body.password, req.body.role, req.body.email, function(err) {
+    if(!err) {
+      console.log('inserted data');
+      res.send('inserted data');
+    } else {
+	console.log(err);
+	res.send('error inserting user');
+    }
+  });
+});
+
+
+app.get('/returnAllNames', function (req, res) {
+  db.users.returnAllNames(function(err, results, fields) {
+    if(!err) {
+      console.log(results);
+      res.send(results);
+    } else {
+        console.log(err);
+	res.send('error retrieving users');
+    }
+  });
+});
+
 
 //Listen on https
 var httpsServer = https.createServer(credentials, app);
